afd8561313eef952869cfca987949cef06af3096
Jack Montgomery <jackm321@gmail.com>
2013-06-14 16:31:14 -0700

    fixed team layout on about page

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-$(function() {});
+$(function() {
+ apply_css_to_team();
+
+  function apply_css_to_team(){
+  /* if chrome use flex boxes */
+  if($.browser.chrome){
+    $(".person, .team_container").each(function(i, target){
+      $(target)
+      .removeClass('no_chrome')
+      .addClass('yes_chrome');
+    });
+  }
+ }
+
+});
